d02a03fc4e49f9bd24fb039e3fad28a210c3a69c
4verBurga <ever.burga.p@uni.pe>
2022-11-28 14:13:59 -0500

    Sticky NavBar: falta añadir padding a body

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import SimpleBar from "simplebar-react";
 import Sidebar from "./Sidebar";
 import CollapsedMenu from "./CollapsedMenu";
-
+import NavBar from "./NavBar";
 import 'simplebar/dist/simplebar.min.css';
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <>  
             <section className="w-full h-full">
+                <NavBar/>
                 <Sidebar/>
                 <CollapsedMenu/>
                 <main className="w-full sm:right-[80px] sm:w-[calc(100%-80px)]">
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+// nav bar that hiddens when size is small
+// and shows when size is large
+
+const NavBar = () => {
+    return (
+        // If navbar is in top change to static and if navbar is in bottom change to sticky
+
+        <>
+            <nav className="fixed top-0 left-0 w-full h-16 bg-white shadow-md z-50">
+                <div className="w-full h-full flex items-center justify-between px-4">
+                    <div className="w-1/2 h-full flex items-center">
+                    </div>
+                </div>
+            </nav>        
+        </>
+    );
+};
+
+export default NavBar;
