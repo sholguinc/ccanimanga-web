091c8fd5355f82504863fe91ecdbcc56d43b2f57
Sergio Holguin <sholguinc@uni.pe>
2022-12-11 20:26:49 -0500

    fixed undefined problems

--- src/pages/talleres/components/WorkshopBanner.jsx
+++ src/pages/talleres/components/WorkshopBanner.jsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 
 const WorkshopBanner = ({ workshopInfo }) => {
+    let bgImage, imageName, title;
+    if (workshopInfo) {
+        bgImage = workshopInfo.backgroundImage
+        imageName = workshopInfo.name
+        title = workshopInfo.title
+    }
+
     return (
         <section className="relative bg-black/20 h-[30vh] sm:h-screen">
             <figure className="h-full m-0 p-0 brightness-50 visible sm:invisible">
                 <Image
-                    src={workshopInfo.backgroundImage}
+                    src={bgImage}
                     layout="fill"
                     objectFit="cover"
-                    alt={workshopInfo.name}
+                    alt={imageName}
                 />
             </figure>
 
             <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center sm:justify-start">
                 <h3 className="font-myFont font-medium text-white m-0 p-0 text-3xl sm:text-5xl sm:ml-[70px] xl:text-6xl ">
-                    {workshopInfo.title}
+                    {title}
                 </h3>
             </div>
         </section>
--- src/pages/talleres/components/WorkshopSummary.jsx
+++ src/pages/talleres/components/WorkshopSummary.jsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 
 const WorkshopSummary = ({ workshopInfo }) => {
+    let summary, image, name;
+    if (workshopInfo) {
+        summary = workshopInfo.summary
+        image = workshopInfo.summaryImage
+        name = workshopInfo.name
+    }
     return (
         <section className="bg-slate-100 py-[30px] sm:p-0">
             <div className="h-min flex items-center justify-around flex-col sm:flex-row">
                 <p className="font-myFont font-medium text-black text-justify text-lg sm:text-xl mx-0 p-0 mt-[10px] mb-[30px]
                 w-[80%] sm:w-[50%] sm:m-10 xl:text-2xl">
-                    {workshopInfo.summary}
+                    {summary}
                 </p>
 
                 <figure className="w-[80%] sm:w-[35%] m-0 aspect-w-2 sm:aspect-w-3 aspect-h-1 m-0 sm:mr-10">
                     <Image
-                        src={workshopInfo.summaryImage}
+                        src={image}
                         layout="fill"
                         objectFit="contain"
-                        alt={workshopInfo.name}
+                        alt={name}
                     />
                 </figure>
             </div>
